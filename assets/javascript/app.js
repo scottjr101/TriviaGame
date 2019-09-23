0f083e461a7e41639d956e5d0ee0abffb5493d08
Scott Ladd <scottjr101@gmail.com>
2019-09-23 17:44:42 -0400

    got game mostly working, need to add timeout for not answering a question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,22 +2,33 @@
 var trivTime = 0;
 var rightCount = 0;
 var wrongCount = 0;
-var questionsCount = 0;
+var unansweredCount = 0;
+var questionsCount = 1;
 var Timer = '';
 var questions = {
     1:{
         question:'What was Metallicas first album?',
-        answers:["Kill em' All", "Reload", "Master of Puppets", "Ride the Lightning"],
+        answers:["Kill 'em All", "Reload", "Master of Puppets", "Ride the Lightning"],
+        correct:"Kill 'em All",
+        right: 'Correct!',
+		wrong: 'Nope!',
+        imageUrl:"https://upload.wikimedia.org/wikipedia/en/thumb/5/5c/Metallica_-_Kill_%27Em_All_cover.jpg/220px-Metallica_-_Kill_%27Em_All_cover.jpg"
     },
     2:{
         question:'What band was in the movie "Ace Ventura Pet Detective"?',
         answers:["Skid Row", "Slayer", "Cannibal Corpse", "Morbid Angel"],
-        correct:"Cannibal Corpse"
+        correct:"Cannibal Corpse",
+        right: 'Correct!',
+		wrong: 'Nope!',
+        imageUrl:"https://thumbs.gfycat.com/BiodegradableUnrulyIberianchiffchaff-size_restricted.gif"
     },
     3:{
         question:'Who has been the "Prince of Darkness" since 1979?',
         answers:["Ronnie Dio", "Ozzy Osbourne", "David Lee Roth", "Donald Trump"],
-        correct:"Ozzy Osbourne"
+        correct:"Ozzy Osbourne",
+        right: 'Correct!',
+		wrong: 'Nope!',
+        imageUrl:"https://media2.giphy.com/media/6SqjWXXtiqeEE/giphy.gif"
     }
 };
 
@@ -52,16 +63,190 @@ var createAnswers = function(){
 		//Create new div to hold answers
 		var newBtn = $('<button>');
 		//Add class to new Div
-		newBtn.addClass('answers redBtn');
+		newBtn.addClass('answers btn btn-dark');
 		//Give buttons attribute
 		newBtn.attr('data-type',answers);
 		//add text to new Div
 		newBtn.text(answers);
 		//Add answers to DOM
-		$('.trivSection').append(newBtn);
+        $('.trivSection').append(newBtn);
+        $('.trivSection').append('<br>');
     }
     //Prevents click event from being saved
 	$(document).off('click','.answers',checkAnswer);
 	$(document).on('click','.answers',checkAnswer);
 }
+var checkAnswer = function(){
+    //Get users answer choice
+   var userAnswer = $(this).data('type');
+   var correctAnswer = questions[questionsCount]['correct'];
+   var correctImg = questions[questionsCount]['imageUrl'];
 
+   var right = questions[questionsCount]['right'];
+   var wrong = questions[questionsCount]['wrong'];
+   console.log(questionsCount);
+   if(userAnswer === correctAnswer){
+       //Update rightCount
+       rightCount++;
+       //Clears out triv Section
+       $('.trivSection').empty();
+       var newImg = $('<img>');
+       newImg.attr('src',correctImg);
+       $('.trivSection').append(newImg);
+       //Create Div
+       var newDiv = $('<div>');
+       //Give div class
+       newDiv.addClass('rightAnswer');
+       //adds CORRECT! text to div
+       newDiv.text(right);
+       //Add answer to DOM
+       $('.trivSection').append(newDiv);
+       //Stops Time
+       clearInterval(timer)
+       //Add 1 to question count to move to the next question
+       questionsCount++;
+       if(questionsCount <= 3){
+           //removes CORRECT! text and continues to create next question after 3 seconds
+           setTimeout(
+               function(){
+                   $('.trivSection').empty();
+                   createQuestions();
+                   },4000);
+       }
+       else{
+           $('.trivSection').empty();
+           var newImg = $('<img>');
+           newImg.attr('src',correctImg);
+           $('.trivSection').append(newImg);
+           //Create Div
+           var newDiv = $('<div>');
+           //Give div class
+           newDiv.addClass('rightAnswer');
+           //adds CORRECT! text to div
+           newDiv.text(right);
+           //Add answer to DOM
+           $('.trivSection').append(newDiv);
+           //Stops Time
+           clearInterval(timer)
+           //Reset
+           setTimeout(gameOver, 4000);
+       }
+   }
+   else{
+       wrongCount++;
+       //Clears out triv Section
+       $('.trivSection').empty();
+       var newImg = $('<img>');
+       newImg.attr('src',correctImg);
+       $('.trivSection').append(newImg);
+       var newDiv = $('<div>');
+       //Give div class
+       newDiv.addClass('wrongAnswer');
+       //adds Wrong! text to div
+       newDiv.html(wrong + '<br>' + 'The Correct Answer was: ' + correctAnswer);
+       //Add answer to DOM
+       $('.trivSection').append(newDiv);
+       //Stops Time
+       clearInterval(timer)
+       //Add 1 to question count to move to the next question
+       questionsCount++;
+       
+       if(questionsCount <= 3){
+           setTimeout(function(){
+           $('.trivSection').empty();
+           createQuestions();
+           },4000);
+       }
+       else{
+           //Clears out triv Section
+           $('.trivSection').empty();
+           var newImg = $('<img>');
+       newImg.attr('src',correctImg);
+       $('.trivSection').append(newImg);
+           var newDiv = $('<div>');
+           //Give div class
+           newDiv.addClass('wrongAnswer');
+           //adds Wrong! text to div
+           newDiv.html(wrong + '<br>' + 'The Correct Answer was: ' + correctAnswer);
+           //Add answer to DOM
+           $('.trivSection').append(newDiv);
+           //Stops Time
+           clearInterval(timer);
+           //Reset
+           setTimeout(gameOver, 4000);
+       }
+   }
+}
+//Timer
+//==========================================
+var timerStart = function(){ 
+   $('.timerSection').empty();
+   //Sets time to 10
+   trivTime = 100;
+   //Progress Bar
+   var timeTag = $('<div>');
+   timeTag.addClass('time');
+   timeTag.addClass('progress');
+   var progressBar = $('<div>');
+   progressBar.addClass('progress-bar');
+   progressBar.width(trivTime + '%');
+
+   $('.timerSection').append(timeTag);
+   $('.time').append(progressBar);	
+   //Decrements Time
+   timer = setInterval(timeDecrement,100);
+}
+var timeDecrement = function(){ 
+   //Progress bar decrement
+   $('.progress-bar').width(trivTime + '%');
+   trivTime--;
+   //if time gets to 0
+   if(trivTime === -10){
+       userAnswer = false;
+       //Clears Time
+       clearInterval(timer);
+       checkAnswer();
+   }
+   
+}
+var gameOver = function(){
+   //Remove everything in trivia section
+   $('.trivSection').empty();
+   //Remove everthing in timer section
+   $('.timerSection').empty();
+   var scoreDiv = $('<div>');
+   scoreDiv.addClass('score');
+   scoreDiv.html('Correct: ' + rightCount + '<br>' + 'Wrong: ' + wrongCount);
+   $('.trivSection').append(scoreDiv);
+   //Assign new div element to new Div
+   var newDiv = $('<div>');
+   //add class to new Div
+   newDiv.addClass('gameOver');
+   //add game over text
+   newDiv.text('Game Over! Play Again ?');
+   //Append game over text to DOM
+   $('.trivSection').append(newDiv);
+   //Create ResetButton
+   var newBtn = $('<button>');
+   //Give btn Class
+   newBtn.addClass('btn btn-dark reset resetBtn');
+   //Give btn reset Text
+   newBtn.text('Reset');
+   //Append
+   $('.trivSection').append(newBtn);
+   //Reset all value
+   trivTime = 100;
+   questionsCount = 1;
+   rightCount = 0;
+   wrongCount = 0;
+   //When reset button is clicked.......
+   $('.resetBtn').on('click',function(){
+       $('.trivSection').empty()
+       //Starts game over
+       createQuestions();
+   });
+}
+
+/*Main
+==============================================================*/
+start();
